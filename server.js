f40589adf98f07fc67f24015695e22a5c4bfe008
christopherchateau <christopherchateau@gmail.com>
2018-12-10 12:49:25 -0700

    Change port to be dynamic, again

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const host = "0.0.0.0";
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 app.use(cors());
 app.use(bodyParser.json());
